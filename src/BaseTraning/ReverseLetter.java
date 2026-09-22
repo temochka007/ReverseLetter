1a7643c7ef7dc082f08a3edb26e0c43eb6ffb611
Artem <zolotuhin.zo1oto@yandex.ru>
2026-09-22 22:34:52 +0300

    На проверкуÐÐ

---
+++ src/BaseTraning/ReverseLetter.java
@@ -0,0 +1,23 @@
+package BaseTraning;
+
+public class ReverseLetter {
+    public static void main(String[] args) {
+        char[] chars = "J@va the be$t!123".toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+        while(left < right){
+            while(left < right && !Character.isLetter(chars[left])){
+                left++;
+            }
+            while(left < right && !Character.isLetter(chars[right])){
+                right--;
+            }
+            char tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+            left++;
+            right--;
+        }
+        System.out.println(new String(chars));
+    }
+}
